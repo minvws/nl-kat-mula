bc001e88568b5191ff5539798a84d49be665c4a7
JP Bruins Slot <jpbruinsslot@gmail.com>
2022-02-01 10:54:24 +0100

    Make test runner work

--- tests/integration/test.py
+++ tests/integration/test.py
@@ -2,5 +2,5 @@ import unittest
 
 
 class Test(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_truthy(self):
+        self.assertEqual(True, True)
