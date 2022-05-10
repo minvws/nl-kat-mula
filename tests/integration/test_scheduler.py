ae6d19484bb1805274b1218a07849c164e7cb847
JP Bruins Slot <jpbruinsslot@gmail.com>
2022-05-10 12:45:33 +0200

    Make initial integration tests work

--- tests/integration/test_scheduler.py
+++ tests/integration/test_scheduler.py
@@ -5,14 +5,20 @@ from types import SimpleNamespace
 from unittest import mock
 
 import scheduler
-from scheduler import config, connectors, context, dispatchers, models
-from tests.factories import BoefjeFactory, BoefjeMetaFactory, OOIFactory, OrganisationFactory, ScanProfileFactory
+from scheduler import (config, connectors, context, dispatchers, models,
+                       queues, rankers, schedulers)
+from tests.factories import (BoefjeFactory, BoefjeMetaFactory, OOIFactory,
+                             OrganisationFactory, ScanProfileFactory)
 
 
 class SchedulerTestCase(unittest.TestCase):
     def setUp(self):
+        cfg = config.settings.Settings()
 
-        # Octopoes
+        self.mock_ctx = mock.patch("scheduler.context.AppContext").start()
+        self.mock_ctx.config = cfg
+
+        # Mock connectors: octopoes
         scan_profile = ScanProfileFactory(level=0)
         ooi = OOIFactory(scan_profile=scan_profile)
 
@@ -21,19 +27,25 @@ class SchedulerTestCase(unittest.TestCase):
             spec_set=True,
         )
 
-        self.mock_octopoes.get_objects.return_value = [ooi]
+        self.mock_octopoes.get_random_objects.return_value = [ooi, None]
+        self.mock_ctx.services.octopoes = self.mock_octopoes
 
-        # Scan profiles
+        # Mock connectors: Scan profiles
+        scan_profile = ScanProfileFactory(level=0)
+        ooi = OOIFactory(scan_profile=scan_profile)
+
         self.mock_scan_profiles = mock.create_autospec(
             spec=connectors.listeners.ScanProfile,
             spec_set=True,
         )
 
-        self.mock_scan_profiles.get_latest_objects.return_value = [ooi]
+        self.mock_scan_profiles.get_latest_object.side_effect = [ooi, None]
 
-        # Katalogus
+        self.mock_ctx.services.scan_profile = self.mock_scan_profiles
+
+        # Mock connectors: Katalogus
         boefje = BoefjeFactory(scan_level=0)
-        self.organisation = OrganisationFactory()
+        organisation = OrganisationFactory()
 
         self.mock_katalogus = mock.create_autospec(
             spec=connectors.services.Katalogus,
@@ -41,13 +53,15 @@ class SchedulerTestCase(unittest.TestCase):
         )
 
         self.mock_katalogus.get_organisations.return_value = [
-            self.organisation,
+            organisation,
         ]
         self.mock_katalogus.get_boefjes_by_ooi_type.return_value = [
             boefje,
         ]
 
-        # Bytes
+        self.mock_ctx.services.katalogus = self.mock_katalogus
+
+        # Mock connectors: Bytes
         self.mock_bytes = mock.create_autospec(
             spec=connectors.services.Bytes,
             spec_set=True,
@@ -58,25 +72,61 @@ class SchedulerTestCase(unittest.TestCase):
             input_ooi=ooi.primary_key,
         )
 
-        # Config
-        cfg = config.settings.Settings()
-
-        # AppContext
-        self.mock_ctx = mock.patch("scheduler.context.AppContext").start()
-
-        self.mock_ctx.services.octopoes = self.mock_octopoes
-        self.mock_ctx.services.katalogus = self.mock_katalogus
         self.mock_ctx.services.bytes = self.mock_bytes
-        self.mock_ctx.services.scan_profile = self.mock_scan_profiles
-        self.mock_ctx.config = cfg
 
-        self.app = scheduler.App(self.mock_ctx)
+        # Scheduler
+        queue = queues.BoefjePriorityQueue(
+            pq_id=organisation.id,
+            maxsize=cfg.pq_maxsize,
+            item_type=models.BoefjeTask,
+            allow_priority_updates=True,
+        )
 
-    @unittest.skip
+        dispatcher = dispatchers.BoefjeDispatcher(
+            ctx=self.mock_ctx,
+            pq=queue,
+            item_type=models.BoefjeTask,
+            celery_queue="boefjes",
+            task_name="tasks.handle_boefje",
+        )
+
+        ranker = rankers.BoefjeRanker(
+            ctx=self.mock_ctx,
+        )
+
+        self.scheduler = schedulers.BoefjeScheduler(
+            ctx=self.mock_ctx,
+            scheduler_id=organisation.id,
+            queue=queue,
+            dispatcher=dispatcher,
+            ranker=ranker,
+            organisation=organisation,
+        )
+
+        mock_create_tasks_for_oois = mock.patch.object(self.scheduler, "create_tasks_for_oois").start()
+        mock_create_tasks_for_oois.side_effect = [
+            [queues.PrioritizedItem(
+                priority=0,
+                item=models.BoefjeTask(
+                    id=uuid.uuid4().hex,
+                    boefje=BoefjeFactory(),
+                    input_ooi=ooi.primary_key,
+                    organization=organisation.id,
+                ),
+            )],
+            [],
+            [],
+            [],
+            [],
+        ]
+
+        # App
+        # self.app = scheduler.App(self.mock_ctx)
+
     def test_populate_boefjes_queue(self):
         """Should populate the boefjes queue with the correct boefje objects"""
-        self.app.schedulers[self.organisation.id].populate_queue()
-        self.assertEqual(len(self.app.schedulers[self.organisation.id].queue), 1)
+        self.scheduler.populate_queue()
+        self.assertEqual(len(self.scheduler.queue), 1)
 
     @unittest.skip
     def test_populate_boefjes_queue_correct_priority(self):
