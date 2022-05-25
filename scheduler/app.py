2ef4c758784cf7fea5e497b078ffea07c8a3ad6c
JP Bruins Slot <jpbruinsslot@gmail.com>
2022-05-25 10:02:20 +0200

    Fix f-string

--- scheduler/app.py
+++ scheduler/app.py
@@ -135,7 +135,7 @@ class App:
 
         scheduler = schedulers.BoefjeScheduler(
             ctx=self.ctx,
-            scheduler_id=f{"boefje-{org.id}"},
+            scheduler_id=f"boefje-{org.id}",
             queue=queue,
             dispatcher=dispatcher,
             ranker=ranker,
