c932d630e2fdd4183df4c8dac197df57f98e85e3
JP Bruins Slot <jpbruinsslot@gmail.com>
2022-08-10 13:04:23 +0200

    Fix spelling

--- scheduler/config/settings.py
+++ scheduler/config/settings.py
@@ -33,12 +33,12 @@ class Settings(BaseSettings):
     host_normalizer_meta: str = Field(..., env="SCHEDULER_RABBITMQ_DSN")
 
     # Queue settings (0 is infinite)
-    pq_maxsize: int = Field(1000, env="SHEDULER_PQ_MAXSIZE")
-    pq_populate_interval: int = Field(60, env="SHEDULER_PQ_INTERVAL")
-    pq_populate_grace_period: int = Field(86400, env="SHEDULER_PQ_GRACE")
+    pq_maxsize: int = Field(1000, env="SCHEDULER_PQ_MAXSIZE")
+    pq_populate_interval: int = Field(60, env="SCHEDULER_PQ_INTERVAL")
+    pq_populate_grace_period: int = Field(86400, env="SCHEDULER_PQ_GRACE")
 
     # Dispatcher settings
-    dsp_interval: int = Field(5, env="SHEDULER_DSP_INTERVAL")
+    dsp_interval: int = Field(5, env="SCHEDULER_DSP_INTERVAL")
     dsp_broker_url: str = Field(..., env="SCHEDULER_DSP_BROKER_URL")
 
     # Database settings
